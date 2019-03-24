f0b4f3f56de5da3da92d2795695f3edd064b5b9f
from_unknown <from.midnight.walker@gmail.com>
2019-03-24 09:23:41 +0900

    簡単なAPIの実装

--- src/Application.kt
+++ src/Application.kt
@@ -1,68 +1,75 @@
 package com.sample
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.html.*
-import kotlinx.html.*
-import kotlinx.css.*
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.sample.model.User
+import com.sample.model.UserRepository
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receiveText
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    val client = HttpClient(Apache) {
-    }
+    val userRepository = UserRepository()
 
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
-        get("/html-dsl") {
-            call.respondHtml {
-                body {
-                    h1 { +"HTML" }
-                    ul {
-                        for (n in 1..10) {
-                            li { +"$n" }
-                        }
-                    }
+        post("/users") {
+            val userJSON = call.receiveText()
+
+            val mapper = jacksonObjectMapper()
+            try {
+                val user = mapper.readValue<User>(userJSON)
+                if (userRepository.add(user)) {
+                    call.respond(HttpStatusCode.OK, "")
+                } else {
+                    call.respond(HttpStatusCode.InternalServerError, "")
                 }
+            } catch (e: Exception){
+                call.respond(HttpStatusCode.InternalServerError, "")
             }
         }
 
-        get("/styles.css") {
-            call.respondCss {
-                body {
-                    backgroundColor = Color.red
+        get("/users") {
+            try {
+                val users = userRepository.findAll()
+                val mapper = jacksonObjectMapper()
+                val usersJSON = mapper.writeValueAsString(users)
+                call.respond(HttpStatusCode.OK, usersJSON)
+            } catch(e: Exception){
+                call.respond(HttpStatusCode.InternalServerError, "")
+            }
+        }
+
+        get("/user/{id}") {
+            try {
+                val user = call.parameters["id"]?.let {
+                    userRepository.findById(it.toInt())
                 }
-                p {
-                    fontSize = 2.em
+                val mapper = jacksonObjectMapper()
+                val userJSON = mapper.writeValueAsString(user)
+                if(userJSON == null){
+                    call.respond(HttpStatusCode.InternalServerError, "")
+                } else {
+                    call.respond(HttpStatusCode.OK, userJSON)
                 }
-                rule("p.myclass") {
-                    color = Color.blue
-                }
+            } catch(e: Exception){
+                call.respond(HttpStatusCode.InternalServerError, "")
             }
         }
     }
 }
 
-fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
-    style(type = ContentType.Text.CSS.toString()) {
-        +CSSBuilder().apply(builder).toString()
-    }
-}
-
-fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
-    this.style = CSSBuilder().apply(builder).toString().trim()
-}
-
-suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
-    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
-}
---
+++ src/model/UserRepository.kt
@@ -0,0 +1,94 @@
+package com.sample.model
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.SQLException
+
+object Users : Table() {
+    val id = integer("id").autoIncrement().primaryKey() // Column<String>
+    val firstName = varchar("firstName", length = 50) // Column<String>
+    val lastName = varchar("lastName", length = 50) // Column<String>
+    val gender = integer("gender") // Column<Int>
+    val age = integer("age") // Column<Int>
+}
+
+class User(val id: Int, val firstName: String, val lastName: String, val gender: Int, val age: Int)
+
+class UserRepository {
+
+    companion object {
+        private const val DEFAULT_USER = "root"
+        private const val DEFAULT_PASSWORD = "secretpassword"
+    }
+
+    var user = DEFAULT_USER
+    var password = DEFAULT_PASSWORD
+    private val connection: Database
+
+    init {
+        connection = Database.connect(
+            "jdbc:mysql://localhost:3306/sample", driver = "com.mysql.jdbc.Driver",
+            user = user, password = password
+        )
+    }
+
+    fun add(user: User): Boolean {
+        return try {
+            transaction(connection) {
+                SchemaUtils.create(Users)
+
+                Users.insert {
+                    it[firstName] = user.firstName
+                    it[lastName] = user.lastName
+                    it[gender] = user.gender
+                    it[age] = user.age
+                }
+            }
+            true
+        } catch (se: SQLException) {
+            se.printStackTrace()
+            false
+        }
+    }
+
+    fun findAll(): List<User> {
+        return try {
+            var userList = ArrayList<User>()
+
+            transaction(connection) {
+                Users.selectAll().forEach {
+                    userList.add(
+                        User(
+                            it[Users.id],
+                            it[Users.firstName],
+                            it[Users.lastName],
+                            it[Users.gender],
+                            it[Users.age]
+                        )
+                    )
+                }
+            }
+            userList
+        } catch (se: SQLException) {
+            se.printStackTrace()
+            throw se
+        }
+    }
+
+    fun findById(id: Int): User {
+        return try {
+            var user = User(-1, "", "", -1, -1)
+            transaction(connection) {
+                Users.select {
+                    Users.id eq id
+                }.forEach {
+                    user = User(it[Users.id], it[Users.firstName], it[Users.lastName], it[Users.gender], it[Users.age])
+                }
+            }
+            user
+        } catch (se: SQLException) {
+            se.printStackTrace()
+            throw se
+        }
+    }
+}
